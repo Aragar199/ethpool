61642e5e32b61e964b67fbef5cb19d144933e939
Aragar199 <avponce@hotmail.com>
2021-09-16 20:23:58 -0700

    more stuff

--- test/EthPool.test.js
+++ test/EthPool.test.js
@@ -10,8 +10,10 @@ require("dotenv").config({path: "../.env"})
 contract("EthPool Test", async(accounts) => {
     const [deployerAccount, recipient, anotherAccount] = accounts
 
-    it("should not have any tokens in my deployerAccount", async () => {
+    it("should have all tokens in my deployerAccount", async () => {
         let instance = await Token.deployed()
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN('10000000000000000000000'))
     })
+
+    
 })
