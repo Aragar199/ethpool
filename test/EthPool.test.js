bece8146e8f15df44c4e2b8455aaae3325b6bbeb
Aragar199 <avponce@hotmail.com>
2021-09-16 20:13:52 -0700

    initial ethpool test

---
+++ test/EthPool.test.js
@@ -0,0 +1,17 @@
+const EthPool = artifacts.require("EthPool")
+const Token = artifacts.require("SimpleToken")
+
+const chai = require("./setupchai.js")
+const BN = web3.utils.BN
+const expect = chai.expect
+
+require("dotenv").config({path: "../.env"})
+
+contract("EthPool Test", async(accounts) => {
+    const [deployerAccount, recipient, anotherAccount] = accounts
+
+    it("should not have any tokens in my deployerAccount", async () => {
+        let instance = await Token.deployed()
+        return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN('10000000000000000000000'))
+    })
+})
