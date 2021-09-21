c69724e8f4401e91424284c391fb3b5aa93dfab9
Aragar199 <avponce@hotmail.com>
2021-09-20 21:11:23 -0700

    refactor and adding notation

--- contracts/EthPool.sol
+++ contracts/EthPool.sol
@@ -4,131 +4,142 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+/**
+* @title EthPool Contract
+* @author alexponce.eth
+* @notice A contract that allows user to farm SimpleToken rewards based on contributions to the pool
+ */
 contract EthPool is Ownable {
 
+    // SimpleToken interface
     IERC20 private _token;
 
-    uint256 public currentSession;
+    // Participants that have contributed
+    address[] participants;
+    
+    // Current total tokens added to the contract as rewards
     uint256 totalRewards;
 
-    address[] participantAddress;
-    mapping (address => uint256) participantIndex;
-
-    struct Session {
-        address[] participants;
-        uint256 sessionDepositTotal;
-        uint256 sessionRewardsTotal;
-        mapping(address => uint256) sessionDeposit;
-        mapping(address => uint256) sessionIndex;
-        mapping(address => bool) isParticipating;
+    // Participant flag and index in participants
+    struct Participant {
+        bool isParticipant;
+        uint256 participantIndex;
     }
     
-    mapping (address => uint256) public balances;
-    mapping (address => uint256) public rewards;
-    mapping (uint256 => Session) public session;
+    // address to Participant information
+    mapping(address => Participant) participant;
 
+    // funds participant will receive when they withdraw
+    mapping(address => uint256) public participantDeposit;
+
+    // rewards participant will receive when they withdraw
+    mapping(address => uint256) public participantReward;
+
+    // EthPool SimpleToken
     constructor (IERC20 token) {
-        _token = token;
+        _token = token;        
     }
 
-    event SuccessfulDeposit(address indexed depositer, uint256 amount);
-    event RewardAdded(address indexed rewarded, uint256 amount);
+    // EthPool Events
+    event DepositAdded(address indexed _to, uint256 _deposit);
+    event RewardAdded(address indexed _to, uint256 _reward);
+    event ParticipantAdded(address indexed _participating);
+    event ParticipantRemoved(address indexed _removed);
 
 
+    /**
+    * @notice EthPool method to deposit funds to the pool
+    */
     function deposit() public payable {
-        emit SuccessfulDeposit(msg.sender, msg.value);
-        balances[msg.sender] += msg.value;
         addParticipant(msg.sender);
-        updateSessionDeposits(msg.sender, msg.value);
+        participantDeposit[msg.sender] += msg.value;
+        emit DepositAdded(msg.sender, msg.value);
     }
 
+    /**
+    * @notice EthPool method to withdraw participant funds and rewards
+    */
     function withdraw() public {
-        uint256 userBalance = balances[msg.sender];
-        balances[msg.sender] = 0;
-        payable(msg.sender).transfer(userBalance);
-        uint256 userReward = rewards[msg.sender];
-        rewards[msg.sender] = 0;
+        uint userDeposit = participantDeposit[msg.sender];
+        participantDeposit[msg.sender] = 0;
+        payable(msg.sender).transfer(userDeposit);
+        uint userReward = participantReward[msg.sender];
+        participantReward[msg.sender] = 0;
         bool sentReward = _token.transfer(msg.sender, userReward);
-        require(sentReward, "Failed to send user rewards to the user");
+        require(sentReward, "Failed to send participant token reward");
         removeParticipant(msg.sender);
     }
-    
-    function currentBalance(address _addr) public view returns(uint256 balance) {
-        return _addr.balance;
-    }
 
-    function addRewards(uint256 _rewards) external payable onlyOwner {
+    /**
+    * @notice EthPool method to add SimpleToken rewards to the pool
+    * @param _rewards to be added to EthPool Contract
+    */
+    function addRewards(uint256 _rewards) external payable onlyOwner{
         address from = msg.sender;
-        (bool sessionRewards) = _token.transferFrom(from, address(this), _rewards);
-        require(sessionRewards, "Failed to add rewards to session");
+        (bool poolRewards) = _token.transferFrom(from, address(this), _rewards);
+        require(poolRewards, "Failed to add rewards to pool");
         totalRewards += _rewards;
-        uint256 numParticipants = session[currentSession].participants.length;
-        
-        for (uint i = 0; i < numParticipants; i++) {
-            address _participant = session[currentSession].participants[0];
-            uint256 _deposit = session[currentSession].sessionDeposit[_participant];
-            uint256 _totalDeposit = session[currentSession].sessionDepositTotal;
-            uint256 _ratio = calculateRatio(_deposit, _totalDeposit);
-            uint256 _participantReward = calculateRewards(_rewards, _ratio);
-            rewards[_participant] += _participantReward;
-            emit RewardAdded(_participant, _participantReward);
-            removeParticipant(_participant);
+        uint numofParticipants = participants.length;
+        for (uint i = 0; i < numofParticipants; i++) {
+            address _participant = participants[i];
+            uint256 _ratio = calculateRatio(participantDeposit[_participant], address(this).balance);
+            uint256 _participantrewards = calculateRewards(_rewards, _ratio);
+            participantReward[_participant] += _participantrewards;
+            emit RewardAdded(_participant, _participantrewards);
         }
-        delete session[currentSession].participants;
-        delete session[currentSession].sessionRewardsTotal; 
-        delete session[currentSession].sessionDepositTotal;
-        currentSession += block.number;
     }
-
-    function startSession() internal {
-
-    }
-
+    
+    /**
+    * @notice EthPool method to track participating address in the pool
+    * @param _addr of participant to be added if not participating
+    */
     function addParticipant(address _addr) internal {
-        if (session[currentSession].isParticipating[msg.sender]) {
+        if (participant[_addr].isParticipant) {
             return;
         } else {
-            session[currentSession].sessionIndex[_addr] = session[currentSession].participants.length;
-            session[currentSession].participants.push(_addr);
-            session[currentSession].isParticipating[msg.sender] = true;
+            uint index = participants.length;
+            participant[_addr].participantIndex = index;
+            participants.push(_addr);
+            participant[_addr].isParticipant = true;
+            emit ParticipantAdded(_addr);
         }
     }
 
+    /**
+    * @notice EthPool method to remove participant from pool
+    * @param _addr of participant to be removed if participating
+    */
     function removeParticipant(address _addr) internal {
-        if (!session[currentSession].isParticipating[_addr]) {
+        if (!participant[_addr].isParticipant) {
             return;
         } else {
-            delete session[currentSession].isParticipating[_addr];
-            delete session[currentSession].sessionDeposit[_addr];
-            uint index = session[currentSession].sessionIndex[_addr];
-            uint lastIndex = session[currentSession].participants.length - 1;
-            address lastKey = session[currentSession].participants[lastIndex];
-
-            session[currentSession].sessionIndex[lastKey] = index;
-            delete session[currentSession].sessionIndex[_addr];
-            session[currentSession].participants.pop();
+            participant[_addr].isParticipant = false;
+            uint index = participant[_addr].participantIndex;
+            uint lastIndex = participants.length - 1;
+            address lastKey = participants[lastIndex];
+            participant[lastKey].participantIndex = index;
+            delete participant[_addr].participantIndex;
+            participants.pop();
+            emit ParticipantRemoved(_addr);
         }
     }
 
-    function removeAddress(address _addr) internal {
-        uint index = participantIndex[_addr];
-        uint lastIndex = participantAddress.length - 1;
-        address lastKey = participantAddress[lastIndex];
-        participantIndex[lastKey] = index;
-        delete participantIndex[_addr];
-        participantAddress.pop();
-    }
-
-    function updateSessionDeposits(address _addr, uint256 _amount) internal {
-        session[currentSession].sessionDepositTotal += _amount;
-        session[currentSession].sessionDeposit[_addr] += _amount;
-    }
+    /**
+    * @notice EthPool method to calculate percentage of funds in pool
+    * @param _deposit funds to be calculated
+    * @param _totalDeposit current total funds in the pool
+    */
     function calculateRatio(uint256 _deposit, uint256 _totalDeposit) internal view returns(uint256 ratio) {
         ratio = (_deposit * 100) / _totalDeposit;
     }
 
+    /**
+    * @notice EthPool method to calculate rewards based on percentage
+    * @param _rewards total rewards to be distributed
+    * @param _ratio percentage of total rewards to be calculated
+    */
     function calculateRewards(uint256 _rewards, uint256 _ratio) internal view returns(uint256 participantRewards) {
         participantRewards = (_rewards * _ratio) / 100;
     }
-
 }
