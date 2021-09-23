0c18529d7fed16c4b32721e56e2091ca97f245f1
Aragar199 <avponce@hotmail.com>
2021-09-22 22:38:53 -0700

    clean up

--- test/setupchai.js
+++ test/setupchai.js
@@ -1,9 +1,9 @@
 "use strict";
-var chai = require("chai");
+const chai = require("chai");
 const BN = web3.utils.BN;
 const chaiBN = require("chai-bn")(BN);
 chai.use(chaiBN);
 
-var chaiAsPromised = require("chai-as-promised");
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 module.exports = chai;
