94f3c7e80ab28afc493c2fb0fb645b21f2a06825
Aragar199 <avponce@hotmail.com>
2021-10-13 12:46:31 -0700

    add more tests

--- test/EthPool.test.js
+++ test/EthPool.test.js
@@ -15,23 +15,14 @@ contract("EthPool Test", async(accounts) => {
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN('10000000000000000000000'))
     })
 
-    it("deployerAccount should be able to add tokens", async () => {
-        let tokenInstance = await Token.deployed()
-        let ethPoolInstance = await EthPool.deployed()
-        let tokenValue = new BN(5)
-        await tokenInstance.increaseAllowance(ethPoolInstance.address, tokenValue, {from: deployerAccount})
-        await ethPoolInstance.addRewards(tokenValue, {from: deployerAccount})
-        return expect(tokenInstance.balanceOf(ethPoolInstance.address)).to.eventually.be.a.bignumber.equal(tokenValue)
-    })
-
     it("participants should be able to deposit ETH", async () => {
         let instance = await EthPool.deployed()
         await instance.deposit({from: participant, value: web3.utils.toWei("1", "wei")})
-        await instance.deposit({from: participant2, value: web3.utils.toWei("3", "wei")})
+        await instance.deposit({from: participant2, value: web3.utils.toWei("300", "wei")})
         expect(instance.deposit({from: participant, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled
-        expect(instance.deposit({from: participant2, value: web3.utils.toWei("3", "wei")})).to.be.fulfilled
+        expect(instance.deposit({from: participant2, value: web3.utils.toWei("300", "wei")})).to.be.fulfilled
         let currentBalance = await web3.eth.getBalance(instance.address)
-        expect(currentBalance).to.be.a.bignumber.equal(new BN("4"))
+        return expect(currentBalance).to.be.a.bignumber.equal(new BN("301"))
     })
 
     it("non participant account should have zero balance, rewards", async () => {
@@ -46,20 +37,31 @@ contract("EthPool Test", async(accounts) => {
         let ethPoolInstance = await EthPool.deployed()
         let participantRatio = await ethPoolInstance.currentRatio(participant)
         let participantRatio2 = await ethPoolInstance.currentRatio(participant2)
-        expect(participantRatio).to.be.a.bignumber.equal("250000000000000000")
-        return expect(participantRatio2).to.be.a.bignumber.equal("750000000000000000")
+        expect(participantRatio).to.be.a.bignumber.equal("3322259136212624")
+        return expect(participantRatio2).to.be.a.bignumber.equal("996677740863787375")
     })
 
     it("should distribute new rewards correctly amongst participants", async () => {
         let tokenInstance = await Token.deployed()
         let ethPoolInstance = await EthPool.deployed()
-        let tokenValue = new BN("200")
+        let tokenValue = new BN("200000000000000000000")
         await tokenInstance.increaseAllowance(ethPoolInstance.address, tokenValue, {from: deployerAccount})
         await ethPoolInstance.addRewards(tokenValue, {from: deployerAccount})
         let reward = await ethPoolInstance.participantReward(participant)
         let reward2 = await ethPoolInstance.participantReward(participant2)
-        expect(reward).to.be.a.bignumber.equal(new BN("50"))
-        return expect(reward2).to.be.a.bignumber.equal(new BN("150"))
+        expect(reward).to.be.a.bignumber.equal(new BN("664451827242524800"))
+        return expect(reward2).to.be.a.bignumber.equal(new BN("199335548172757475000"))
+    })
+
+    it("participants should be able to withdraw rewards", async () => {
+        let tokenInstance = await Token.deployed()
+        let ethPoolInstance = await EthPool.deployed() 
+        await ethPoolInstance.withdraw({from: participant})
+        await ethPoolInstance.withdraw({from: participant2})
+        let participantBalance = await tokenInstance.balanceOf(participant) 
+        let participantBalance2 = await tokenInstance.balanceOf(participant2)
+        expect(participantBalance).to.be.a.bignumber.equal(new BN("664451827242524800"))
+        return expect(participantBalance2).to.be.a.bignumber.equal(new BN("199335548172757475000"))
     })
 
     
