4302cc5c974acb4a33b30a708992f62a18f54213
Aragar199 <avponce@hotmail.com>
2021-10-13 12:22:17 -0700

    add tests for rewards distribution

--- test/EthPool.test.js
+++ test/EthPool.test.js
@@ -8,7 +8,7 @@ const expect = chai.expect
 require("dotenv").config({path: "../.env"})
 
 contract("EthPool Test", async(accounts) => {
-    const [deployerAccount, participant, anotherAccount] = accounts
+    const [deployerAccount, participant, participant2, nonparticipant] = accounts
 
     it("should have all tokens in my deployerAccount", async () => {
         let instance = await Token.deployed()
@@ -24,21 +24,42 @@ contract("EthPool Test", async(accounts) => {
         return expect(tokenInstance.balanceOf(ethPoolInstance.address)).to.eventually.be.a.bignumber.equal(tokenValue)
     })
 
-    it("participant should be able to deposit ETH", async () => {
+    it("participants should be able to deposit ETH", async () => {
         let instance = await EthPool.deployed()
         await instance.deposit({from: participant, value: web3.utils.toWei("1", "wei")})
+        await instance.deposit({from: participant2, value: web3.utils.toWei("3", "wei")})
         expect(instance.deposit({from: participant, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled
+        expect(instance.deposit({from: participant2, value: web3.utils.toWei("3", "wei")})).to.be.fulfilled
         let currentBalance = await web3.eth.getBalance(instance.address)
-        expect(currentBalance).to.be.a.bignumber.equal(new BN('1'))
+        expect(currentBalance).to.be.a.bignumber.equal(new BN("4"))
     })
 
-    it("anotherAccount should have zero balance, rewards", async () => {
+    it("non participant account should have zero balance, rewards", async () => {
+        let ethPoolInstance = await EthPool.deployed()
+        let reward = await ethPoolInstance.participantReward(nonparticipant)
+        let deposit = await ethPoolInstance.participantDeposit(nonparticipant)
+        expect(reward).to.be.a.bignumber.equal(new BN("0"))
+        return expect(deposit).to.be.a.bignumber.equal(new BN("0"))
+    })
+
+    it("should calculate ratio correctly amongst participants", async () => {
+        let ethPoolInstance = await EthPool.deployed()
+        let participantRatio = await ethPoolInstance.currentRatio(participant)
+        let participantRatio2 = await ethPoolInstance.currentRatio(participant2)
+        expect(participantRatio).to.be.a.bignumber.equal("250000000000000000")
+        return expect(participantRatio2).to.be.a.bignumber.equal("750000000000000000")
+    })
+
+    it("should distribute new rewards correctly amongst participants", async () => {
         let tokenInstance = await Token.deployed()
         let ethPoolInstance = await EthPool.deployed()
-        let reward = await ethPoolInstance.participantReward(anotherAccount)
-        let deposit = await ethPoolInstance.participantDeposit(anotherAccount)
-        expect(reward).to.be.a.bignumber.equal(new BN(0))
-        return expect(deposit).to.be.a.bignumber.equal(new BN(0))
+        let tokenValue = new BN("200")
+        await tokenInstance.increaseAllowance(ethPoolInstance.address, tokenValue, {from: deployerAccount})
+        await ethPoolInstance.addRewards(tokenValue, {from: deployerAccount})
+        let reward = await ethPoolInstance.participantReward(participant)
+        let reward2 = await ethPoolInstance.participantReward(participant2)
+        expect(reward).to.be.a.bignumber.equal(new BN("50"))
+        return expect(reward2).to.be.a.bignumber.equal(new BN("150"))
     })
 
     
